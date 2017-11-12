9d58a169d1c9921c3f414db4ba4fd92c20d924f0
15529343201 <15529343201@139.com>
2017-11-12 22:09:38 +0800

    add mxonline

---
+++ MxOnline-v6/apps/users/urls.py
@@ -0,0 +1,18 @@
+# _*_ coding:utf-8 _*_
+__author__ = 'admin'
+
+from django.conf.urls import url,include
+from .views import UserinfoView,UploadImageView,UpdatePwdView,SendEmailCodeView,UpdateEmailView
+
+urlpatterns = [
+    #用户信息
+    url(r'^info/$',UserinfoView.as_view(),name="user_info"),
+    #用户头像上传
+    url(r'^image/upload/$',UploadImageView.as_view(),name="image_upload"),
+    #用户个人中心修改密码
+    url(r'^update/pwd/$',UpdatePwdView.as_view(),name="update_pwd"),
+    #发送邮箱验证码
+    url(r'^sendmail_code/$',SendEmailCodeView.as_view(),name="sendmail_code"),
+    #修改邮箱
+    url(r'^update_email/$',UpdateEmailView.as_view(),name="update_email")
+]
